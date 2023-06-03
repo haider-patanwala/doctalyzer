6854df20443d100d84bb8d4edd346aa85ab3d95e
haider-patanwala <haiderpatanwala@hotmail.com>
2023-06-04 01:19:35 +0530

    Styled Buttons and added routes

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -4,8 +4,8 @@ import "./buttons.css"
 
 const Buttons = () => {
 	return (
-		<div className='flex flex-row justify-between w-full mt-12'>
-			<div className='flex flex-col p-5 items-center w-full max-w-xl p-12 mx-5 rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg'>
+		<div className='flex flex-row justify-around w-full mt-12'>
+			<div className='flex flex-col p-5 items-center w-64 max-w-xl p-12 mx-2 rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg'>
 				<Link to='/ocr'>
 					<div>
 						<img
@@ -17,11 +17,11 @@ const Buttons = () => {
 					<div className='flex justify-center'>Click Me</div>
 				</Link>
 			</div>
-			<div className='flex flex-col p-5 items-center w-full max-w-xl p-12 mx-5 rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg'>
-				<Link to='/ocr'>
+			<div className='flex flex-col p-5 items-center w-64 max-w-xl p-12 mx-2 rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg'>
+				<Link to='/medical'>
 					<div>
 						<img
-							src='../src/assets/upload.png'
+							src='../src/assets/medicine.png'
 							alt='upload'
 							className='button-img p-5'
 						/>
